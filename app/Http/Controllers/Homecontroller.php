5536f7b33092cef1c92febc40222bea60e889740
JerryRockCat <94103010+JerryRockCat@users.noreply.github.com>
2022-10-09 10:43:38 +0700

    ecomt

---
+++ app/Http/Controllers/Homecontroller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class Homecontroller extends Controller
+{
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+
+        if($usertype=='1')
+        {
+            return view('admin.home');
+        }
+
+        else
+        {
+            return view('user.home');
+        }
+    }
+
+    public function index()
+    {
+        if(Auth::id())
+        {
+            return redirect('redirect');
+        }
+        else
+        {
+            return view('user.home');    
+        }
+        
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+  </head>
+  <body>
+    
+      <!-- partial -->
+      
+      @include('admin.sidebar')
+
+      @include('admin.navbar')
+
+
+        <!-- partial -->
+       @include('admin.body')
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    
+      @inclue('admin.script')
+
+  </body>
+</html>
